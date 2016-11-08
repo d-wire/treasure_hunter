36058fe50d9a4d1a931ded2d14931c250456f32d
okkhoury <okk6nb@virginia.edu>
2016-11-07 20:11:34 -0500

    Added code to save username locally using sharedPreferences. When the user hits the continue button on the first page, it'll save the username

--- app/src/main/java/cs4720/cs4720finalproject/Activity/FirstStartupActivity.java
+++ app/src/main/java/cs4720/cs4720finalproject/Activity/FirstStartupActivity.java
@@ -1,29 +1,57 @@
 package cs4720.cs4720finalproject.Activity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import cs4720.cs4720finalproject.R;
 import cs4720.cs4720finalproject.Model.User;
 
 public class FirstStartupActivity extends AppCompatActivity {
 
+
+    private SharedPreferences sharedPreferences;
+
+    public static final String MyPREFERENCES = "MyPrefs" ;
+    public static final String usernameKey = "usernameKey";
+    public static final String iconKey = "iconKey";
+
     private EditText editText;
     private String username;
+    private Button continueButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Instantiate widgets
         editText = (EditText) findViewById(R.id.editText);
+        continueButton = (Button)findViewById(R.id.button);
+
+        continueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveLocally(editText.toString());
+            }
+        });
+
     }
 
     // Button onClick method to save the username and profile icon to the local database
-    public void saveToDB() {
-        if(!editText.getText().equals("")) {
-            username = editText.getText().toString();
-            User me = new User(username);
-        }
+    public void saveLocally(String username) {
+        sharedPreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(usernameKey, username);
+
+//        if(!editText.getText().equals("")) {
+//            username = editText.getText().toString();
+//            User me = new User(username);
+//        }
     }
 }
